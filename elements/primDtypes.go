0b2b92333e7690ad460c3a1f765d4297e9ed81fe
Shubham Dhapola <dhapola.shubham321@gmail.com>
2023-08-22 15:01:00 +0530

    Add primitive data-structures : add support for Set data-type using map-hack

---
+++ elements/primDtypes.go
@@ -0,0 +1,12 @@
+package elements
+
+// using map with boolean values to enact as 'Set'
+// data-structure.
+var validCategoryModel = map[string]bool{
+	"Supervised-Learning":      false,
+	"Unsupervised-Learning":    true,
+	"Semi-Supervised-Learning": true,
+	"Self-Supervised-Learning": true,
+	"Reinforcement-Learning":   false,
+	"One-shot-Learners":        false,
+}
